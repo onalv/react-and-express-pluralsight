387ca4ed1f0c657a2ef76a577b427111af1da5c3
onalv <omarnavaalvarez@gmail.com>
2018-02-09 18:42:58 -0500

    added delete

--- app/stores/GroceryItemStore.jsx
+++ app/stores/GroceryItemStore.jsx
@@ -18,6 +18,18 @@ function GroceryItemStore() {
         triggerListeners();
     }
 
+    function deleteGroceryItem(item) {
+        var index;
+        items.filter(function(_item, _index) {
+            if (_item.name == item.name) {
+                index = _index;
+            }
+        });
+
+        items.splice(index, 1);
+        triggerListeners();
+    }
+
     function onChange(listener) {
         listeners.push(listener);
     }
@@ -34,6 +46,9 @@ function GroceryItemStore() {
             switch (split[1]) {
                 case "add":
                     addGroceryItem(event.payload);
+                    break;
+                case "delete":
+                    deleteGroceryItem(event.payload);
                     break;
             }
         }
